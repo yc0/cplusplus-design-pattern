b1ef7bf3efe723cdde34bd48fe464448f94ee802
yc0 <nelson.lin@pushme.com.tw>
2018-08-20 07:47:40 +0800

    Initial Commit

---
+++ design-pattern/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  design-pattern
+//
+//  Created by nelson lin on 8/20/18.
+//  Copyright © 2018 nelson lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
